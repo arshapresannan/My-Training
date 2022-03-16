d747a444d5536ecd86903ce4e5fd1a32ab8f3a2c
arshapresannan <101731042+arshapresannan@users.noreply.github.com>
2022-03-16 22:32:56 +0530

    Uploading my project to github repository

---
+++ SampleMaven/src/main/java/excelcode/ExcelMain.java
@@ -0,0 +1,20 @@
+package excelcode;
+
+import java.io.IOException;
+
+public class ExcelMain 
+{
+
+	public static void main(String[] args) throws IOException
+	
+	{
+		
+		Excel ob =new Excel();
+		String a=ob.readData(0,0);
+		System.out.println("values of a is "+a);
+		String b=ob.readData(0,1);
+		System.out.println("values of b is "+b);
+		
+	}
+
+}
